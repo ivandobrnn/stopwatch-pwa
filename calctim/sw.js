b3af7d9883ad07371a5416ecb128c2a222fbbba8
Ivan <dutcevivannn@gmail.com>
2026-04-26 19:27:37 +0300

    calctim: pin bottom nav to screen bottom across devices

--- calctim/sw.js
+++ calctim/sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "calctim-pwa-v2";
+﻿const CACHE_NAME = "calctim-pwa-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -61,4 +61,5 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
